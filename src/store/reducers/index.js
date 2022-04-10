911b668781b876113c37606094d3de5a33f4bb70
Den081985 <pishnenkod@gmail.com>
2022-04-10 13:03:14 +0300

    Updates

--- src/store/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import favouriteReducer from "./favouriteReducer";
-//combineReducers объединяет несколько редьюсеров в один объект
-export const rootReducer = combineReducers({
-  favouriteReducer,
-});
